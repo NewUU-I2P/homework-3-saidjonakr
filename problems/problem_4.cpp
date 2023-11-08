b98871c4b2f88ac9fd0d44c56ad1ac3fd83c0939
saidjonakr <saidjonakr@gmail.com>
2023-11-08 17:53:54 +0500

    qwerty

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -4,18 +4,18 @@
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
     int n = 0;
-    if (&macAddress[0] == 'F' and &macAddress[1] == 'F' ){
+    if (macAddress[0] == 'F' and macAddress[1] == 'F' ){
         return "Broadcast";
     }else{
-        if (&macAddress[0] >= '0' and &macAddress[0] <= '0'){
-            n += (&macAddress[0] - '0') * 16;
+        if (macAddress[0] >= '0' and macAddress[0] <= '0'){
+            n += (macAddress[0] - '0') * 16;
         }else {
-            n += (&macAddress[0] - 'A' + 10) * 16;
+            n += (macAddress[0] - 'A' + 10) * 16;
         }
-        if (&macAddress[1] >= '0' and &macAddress[1] <= '0'){
-            n += (&macAddress[1] - '0');
+        if (macAddress[1] >= '0' and macAddress[1] <= '0'){
+            n += (macAddress[1] - '0');
         }else {
-            n += (&macAddress[1] - 'A' + 10);
+            n += (macAddress[1] - 'A' + 10);
         }
         if(n % 2 == 0){
             return "Unicast";
