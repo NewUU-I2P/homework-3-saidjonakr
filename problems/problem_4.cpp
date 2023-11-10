0bc7971f54a6314dec66213fc518dc7aa183777f
saidjonakr <saidjonakr@gmail.com>
2023-11-10 11:07:03 +0500

    qwerty

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -4,25 +4,27 @@
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
     int n = 0;
+    string ans;
     if (macAddress[0] == 'F' and macAddress[1] == 'F' ){
-        return "Broadcast";
-    }else{
-        if (macAddress[0] >= '0' and macAddress[0] <= '0'){
+        ans =  "Broadcast";
+    }else {
+        if (macAddress[0] >= '0' and macAddress[0] <= '0') {
             n += (macAddress[0] - '0') * 16;
-        }else {
+        } else {
             n += (macAddress[0] - 'A' + 10) * 16;
         }
-        if (macAddress[1] >= '0' and macAddress[1] <= '0'){
+        if (macAddress[1] >= '0' and macAddress[1] <= '0') {
             n += (macAddress[1] - '0');
-        }else {
+        } else {
             n += (macAddress[1] - 'A' + 10);
         }
-        if(n % 2 == 0){
-            return "Unicast";
-        }else{
-            return "Multicast";
+        if (n % 2 == 0) {
+            ans = "Unicast";
+        } else {
+            ans = "Multicast";
         }
     }
+    return ans;
     // make use of control flow statements
     // return result;
 }
--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -10,8 +10,10 @@ float problemSolution5(float x, float y, char operation) {
             break;
         case '+':
             result = x + y;
+            break;
         case '-':
             result = x - y;
+            break;
     }
    return result;
 }
