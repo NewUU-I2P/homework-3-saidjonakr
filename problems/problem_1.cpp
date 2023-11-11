f7de6ee93861b495008d7722c90e13bb9a995053
saidjonakr <saidjonakr@gmail.com>
2023-11-11 19:52:34 +0500

    qwerty

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -2,19 +2,18 @@ float problemSolution1(float consumed_water) {
     float cost = 13.0;
     // write your code here
     if(consumed_water > 0.0){
-        cost = 13.0
-        cost += (consumed_water > 30.0 ? 30.0 : consumed_water) * 0.4;
+        cost += (consumed_water >= 30.0 ? 30.0 : consumed_water) * 0.4;
         consumed_water -= 30.0;
     }
     if(consumed_water > 0.0){
-        cost += (consumed_water > 20.0 ? 20.0 : consumed_water) * 0.12;
+        cost += (consumed_water >= 20.0 ? 20.0 : consumed_water) * 0.12;
         consumed_water -= 20.0;
     }
     if(consumed_water > 0.0){
-        cost += (consumed_water > 10.0 ? 10.0 : consumed_water) * 1.4;
+        cost += (consumed_water >= 10.0 ? 10.0 : consumed_water) * 1.4;
         consumed_water -= 10.0;
     }
-    if(consumed_water > 0.0){
+    if(consumed_water >= 0.0){
         cost += consumed_water * 1.5;
     }
     return cost;
