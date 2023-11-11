77c2b03d2ab78845e82b9bc8b543c65f600d06b9
saidjonakr <saidjonakr@gmail.com>
2023-11-11 20:45:15 +0500

    qwerty

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,60 +1,9 @@
 double problemSolution1(double consumed_water) {
     // write your code here
-/*
-    double cost = 13.0;
-    if(consumed_water > 0.0) {
-        if(consumed_water >= 30.0){
-            cost += 30.0 * 0.4;
-        }else {
-            cost += consumed_water * 0.4;
-        }
-        consumed_water -= 30.0;
-    }
-    if(consumed_water > 0.0) {
-        if(consumed_water >= 20.0){
-            cost += 20.0 * 0.12;
-        }else {
-            cost += consumed_water * 0.12;
-        }
-        consumed_water -= 20.0;
-    }
-    if(consumed_water > 0.0) {
-        if(consumed_water >= 20.0){
-            cost += 10.0 * 1.4;
-        }else {
-            cost += consumed_water * 1.4;
-        }
-        consumed_water -= 10.0;
-    }
-    if(consumed_water > 0.0) {
-        cost += consumed_water * 1.5;
-    }
-    return cost;
-*/
-    double fixedAmount = 13.0, consumption = consumed_water;
-
-    // Prices per cubic meter
-    double price1 = 0.4;
-    double price2 = 0.12;
-    double price3 = 1.4;
-    double priceAdditional = 1.5;
-
-    // Consumption thresholds
-    double threshold1 = 30;
-    double threshold2 = 50;
-    double threshold3 = 60;
-
-    // Calculate cost based on consumption
-    double cost;
-    if (consumption <= threshold1) {
-        cost = fixedAmount + consumption * price1;
-    } else if (consumption <= threshold2) {
-        cost = fixedAmount + threshold1 * price1 + (consumption - threshold1) * price2;
-    } else if (consumption <= threshold3) {
-        cost = fixedAmount + threshold1 * price1 + (threshold2 - threshold1) * price2 + (consumption - threshold2) * price3;
-    } else {
-        cost = fixedAmount + threshold1 * price1 + (threshold2 - threshold1) * price2 + (threshold3 - threshold2) * price3 + (consumption - threshold3) * priceAdditional;
-    }
-
+    double cost = 0;
+    cost = (consumed_water <= 30 ? 13 + (consumed_water * 0.4) : 13);
+    cost = (((consumed_water > 30) && (consumed_water <= 50)) ? 13 + 30 * 0.4 + (consumed_water - 30) * 0.12 : 13);
+    cost = (((50 < consumed_water) && (consumed_water <= 60)) ? 13 + 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4 : 13);
+    cost = ((consumed_water >= 60) ? 13 + 30 * 0.4 + 20* 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.5 : 13);
     return cost;
 }
