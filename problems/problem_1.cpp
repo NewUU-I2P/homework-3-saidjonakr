f38983bd4a90025fe60d1fd81b9668f6eba1863f
saidjonakr <saidjonakr@gmail.com>
2023-11-11 20:50:12 +0500

    qwerty

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,9 +1,17 @@
 double problemSolution1(double consumed_water) {
     // write your code here
-    double cost = 0;
-    cost = (consumed_water <= 30 ? 13 + (consumed_water * 0.4) : 13);
-    cost = (((consumed_water > 30) && (consumed_water <= 50)) ? 13 + 30 * 0.4 + (consumed_water - 30) * 0.12 : 13);
-    cost = (((50 < consumed_water) && (consumed_water <= 60)) ? 13 + 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4 : 13);
-    cost = ((consumed_water >= 60) ? 13 + 30 * 0.4 + 20* 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.5 : 13);
+    double cost;
+    if (consumed_water <= 30) {
+        cost = 13 + (consumed_water * 0.4);
+    }
+    if ((consumed_water > 30) and (consumed_water <= 50))
+        cost = 13 + 30 * 0.4 + (consumed_water - 30) * 0.12;
+
+    if ((50 < consumed_water) and (consumed_water <= 60)) {
+        cost = 13 + 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
+    }
+    if (consumed_water >= 60) {
+        cost = 13 + 30 * 0.4 + 20* 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.5 ;
+    }
     return cost;
 }
